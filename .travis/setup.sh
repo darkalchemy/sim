9e9cd69af0fad5425d1b79bca8b39e76a6ff6736
SimonDevelop <contact@simon-micheneau.fr>
2018-12-05 17:49:28 +0100

    build: Update travis organisation

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -3,9 +3,11 @@
 if [ $DB == 'sqlite' ] || [ $DB == 'postgres' ]; then
   if [ $DB == 'postgres' ]; then
     psql -c 'create database sim_prod;' -U postgres
-    cp .env.postgresql .env
+    cp .travis/.env.postgresql .env
+  else
+    cp .travis/.env.sqlite .env
   fi
 else
   sh -c "mysql -e 'CREATE DATABASE sim_prod;'"
-  cp .env.mysql .env
+  cp .travis/.env.mysql .env
 fi
